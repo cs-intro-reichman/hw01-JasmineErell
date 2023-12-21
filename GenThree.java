86f25826ee0ba82b7edfee2a5ca469ab6777f584
JasmineErell <jasmine.erell@gmail.com>
2023-12-21 15:56:08 +0200

    my first HW

--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,19 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+import java.util.Random;
+class Gen {
+    public static void main(String[] args) {
+        Random rand = new Random();
+    	int a = Integer.parseInt(args[0]);
+	    int b = Integer.parseInt(args[1]);
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        int ran1 = rand.nextInt(max - min + 1) + min;
+        int ran2 = rand.nextInt(max - min + 1) + min;
+        int ran3 = rand.nextInt(max - min + 1) + min;
+        System.out.println(ran1);
+        System.out.println(ran2);
+        System.out.println(ran3);
+        int min1 = Math.min(ran1, ran2);
+        int miniest = Math.min(min1, ran3);
+        System.out.println("the smallest number is " + miniest);
+    }
 }
