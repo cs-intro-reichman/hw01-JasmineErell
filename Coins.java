7b651c2a99a56c5577652845f5d7d0e36be14153
JasmineErell <jasmine.erell@gmail.com>
2023-12-21 19:21:28 +0200

    fix 3

--- Coins.java
+++ Coins.java
@@ -1,6 +1,6 @@
 class Coins {
     public static void main(String[] args) {
     	int Cnum = Integer.parseInt(args[0]);
-        System.out.println("use" + " " + Cnum/25 + " " + "quarters and" + " " + Cnum%25 + " " +"cent");
+        System.out.println("Use" + " " + Cnum/25 + " " + "quarters and" + " " + Cnum%25 + " " +"cents");
     }
 }
--- LinearEq.java
+++ LinearEq.java
@@ -4,6 +4,6 @@ class LinearEq {
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
         System.out.println(a + " * x " + "+" + " " + b + " " + "=" + " " + c);
-        System.out.println("x=" + (c-b)/a);
+        System.out.println("x="+(c-b)/a);
     }
 }
