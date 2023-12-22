d115061e9aa016ec26b7d368648c3582c1b8ca97
JasmineErell <jasmine.erell@gmail.com>
2023-12-22 15:27:47 +0200

    I hope its final

--- GenThree.java
+++ GenThree.java
@@ -3,10 +3,10 @@ class GenThree {
     public static void main(String[] args) {
         Random rand = new Random();
     	int min = Integer.parseInt(args[0]);
-	int max = Integer.parseInt(args[1]);
-        int ran1 = ((int)(Math.random()*max) + min);
-        int ran2 = ((int)(Math.random()*max) + min);
-        int ran3 = ((int)(Math.random()*max) + min);
+	    int max = Integer.parseInt(args[1]);
+        int ran1 = ((int)( Math.random()*(max - min + 1) ) + min);
+        int ran2 = ((int)( Math.random()*(max - min + 1) ) + min);
+        int ran3 = ((int)( Math.random()*(max - min + 1) ) + min);
         System.out.println(ran1);
         System.out.println(ran2);
         System.out.println(ran3);
